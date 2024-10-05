6d8182ec170ef2ef61e24b0efa3b52fd701358b8
Dior.Shelton <dior.shelton@gmail.com>
2024-10-05 16:49:54 -0400

    add error handling to getPostUpdate controller, fix post.ejs error that didn't allow message field to populate upon edit

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,4 +1,5 @@
 const Post = require("../models/Post");
+const parseVErr = require("../utils/parseValidationErrs")
 
 const getAllPosts = async (req, res) => {
 	const posts = await Post.find({ createdBy: req.user._id });
@@ -6,7 +7,13 @@ const getAllPosts = async (req, res) => {
 };
 
 const submitNewPost = async (req, res) => {
-	res.send("add a new post");
+  	const {
+			body: { title, message },
+			user: { _id: userId },
+		} = req;
+	console.log( userId, title, message)
+  const post = await Post.create()
+  // res.send("add a new post");
 };
 
 const getPostForm = async (req, res) => {
@@ -37,21 +44,23 @@ const getPostUpdate = async (req, res) => {
 		req.flash("error", "Title or Message fields cannot be empty");
 	}
 
-	try {
-		await Post.findByIdAndUpdate({ _id: postId, createdBy: userId }, req.body, {
-			new: true,
-			runValidators: true,
-		});
-		req.flash("info", "Post Updated.");
-		getAllPosts(req, res);
-	} catch (error) {
-		if (error) {
-			req.flash("error", error._message);
-		} else {
-			return next(error);
-		}
-		return res.render("post", { post: post, errors: req.flash("error") });
-	}
+  try {
+    await Post.findByIdAndUpdate({ _id: postId, createdBy: userId }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    req.flash("info", "Post Updated.");
+    getAllPosts(req, res);
+  } catch (error) {
+    if (error.constructor.name === "ValidationError") {
+      parseVErr(error, req)
+    } else if (error) {
+      req.flash("error", error._message);
+    } else {
+      return next(error)
+    }
+    return res.render("post", { post: post, errors: req.flash("error") });
+  }
 };
 
 const deletePost = async (req, res) => {
